ccf70d76cf1664155213389c246bf6a23635b65c
NiVanc <nico@izebra.be>
2020-04-10 16:05:20 +0200

    Pass a dynamic title with slots

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-xs-12 vcenter">
         <app-quote>
-          <h2>The quote</h2>
+          <h2>{{ quoteTitle }}</h2>
           <p>A wonderful quote!</p>
         </app-quote>
       </div>
@@ -15,6 +15,11 @@
 import Quote from "./components/Quote.vue";
 
 export default {
+  data() {
+    return {
+      quoteTitle: "The quote"
+    };
+  },
   components: {
     "app-quote": Quote
   }
