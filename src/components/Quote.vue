d5069536d293d3d4e2fa2f251f09db69063fc872
NiVanc <nico@izebra.be>
2020-04-10 14:00:41 +0200

    Set up and use a Quote component

---
+++ src/components/Quote.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>A wonderful quote</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 5px #ddd;
+  margin: 75px auto;
+  padding: 35px;
+  text-align: center;
+}
+
+p {
+  margin: 0;
+}
+</style>
